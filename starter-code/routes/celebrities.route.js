0f27348aec83491960f7539f4ea836b87c5be887
Arnoldo Sicilia <arnoldo.sicilia.labarquilla@gmail.com>
2020-02-08 18:55:15 +0100

    Hecho todo la celebrity

--- starter-code/routes/celebrities.route.js
+++ starter-code/routes/celebrities.route.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 const Celebrity = require('../models/celebrity.model')
 
+
+
 // GET CELEBRITIES LIST 
 
 router.get('/index', (req, res, ) => {
@@ -14,26 +16,61 @@ router.get('/index', (req, res, ) => {
     .catch(err => console.log(err))
 })
 
-// INSET A NEW CELEBRITY IN THE DB
+
+
+// INSERT A NEW CELEBRITY IN THE DB
 
 router.get('/new', (req, res) => res.render('celebrities/newCelebrity'))
 router.post('/new', (req, res) => {
 
   const { name, occupation, catchPhrase } = req.body
   Celebrity.create({ name, occupation, catchPhrase })
-    .then(newCelebrity => res.redirect('/celebrities/index'))
+    .then(() => res.redirect('/celebrities/index'))
     .catch(err=>console.log(err))
 })
+
+
 
 // GET CELEBRITY DETAILS  
 
 router.get('/:id', (req ,  res) => {
     
     Celebrity.findById(req.params.id)
-      .then(cebelityFromDB => res.render('celebrities/celebrityDetails', cebelityFromDB))
+      .then(celebrityFromDB => res.render('celebrities/celebrityDetails', celebrityFromDB))
+      .catch(err => console.log(err))
 })
 
+
+
+// DELETE CELEBRITY
+
+router.get('/:id/delete', (req, res) => {
+
+  Celebrity.findByIdAndDelete(req.params.id)
+    .then(() => res.redirect('/celebrities/index'))
+    .catch(err => console.log(err))
+})
+
+
+
+// MODIFY CELEBRITY
+
+router.get('/:id/edit', (req, res) => {
   
+    Celebrity.findById(req.params.id)
+    .then(celebrityFromDB => res.render('celebrities/celebrityModifyForm', celebrityFromDB))
+    .catch(err => console.log(err))
+}) 
+
+router.post('/:id', (req, res) => {
+
+  const { name, occupation, catchPhrase } = req.body
+  Celebrity.findByIdAndUpdate({ name, occupation, catchPhrase })
+    .then(() => res.redirect('/celebrities/index'))
+    .catch(err=>console.log(err))
+})
+
+
 
 
 module.exports = router;
